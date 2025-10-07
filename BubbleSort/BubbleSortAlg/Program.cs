2d0fb1e8560e8fa0ba3b2b242cd734e971fe7409
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-07 09:42:41 +0300

    Create Program.cs

---
+++ BubbleSort/BubbleSortAlg/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program{
+     static void BubbleSort(int[] arr){
+        for(int i = 0; i < arr.Length - 1; i++){
+            for(int j = 0; j < arr.Length - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    (arr[j], arr[j+1]) = (arr[j + 1], arr[j]);
+                }
+            }
+        }
+    }
+
+
+    static void Main(){
+        int[] arr = {8, 5, 6, 4, 3, 6, 8, 9};
+        BubbleSort(arr);
+        foreach(int i in arr){
+            Console.Write(i + " ");
+        }
+
+    }
+}
